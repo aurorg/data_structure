61c5c95b87c0f23f8c92cce52de352706031b6a1
aurorg <2519652258@qq.com>
2022-11-09 21:43:17 +0800

    1

--- classtest/a_pta/two/7.c
+++ classtest/a_pta/two/7.c
@@ -1,14 +1,31 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-
 typedef struct node {
     int coef;
     int expn;
     struct node *next;
 }Node, *List;
+//声明
+List CreatList(int i);
+ 
+List Plus(List head1, List head2);
+List Multiply (List head1, List head2);
 
-List Creat(int i) {
+void Print(List head);
+List CopyList (List head);
+
+int main() {
+    int ner1,nert2;
+    List head1, head2;
+    scanf(" %d", &ner1);
+    head1 = CreatList(ner1);
+    scanf("%d", &nert2);
+    head2 = CreatList(nert2);
+    List head = Multiply(head1, head2);
+    Print(head);
+}
+List CreatList(int i) {
     List head;
     head = (List)malloc(sizeof(Node));
     List pre, pnew;
@@ -22,51 +39,41 @@ List Creat(int i) {
     }
     return head;
 }
-
 void Print(List head) {
-    List q = head->next->next;
-    int flag = 1;
-    if(!q) {
-        putchar('0');
-        printf("\n");
-        return;
-    }
-    while(q) {
-        if(q->coef > 0 && flag != 1) {
-            putchar('+');
+    Node *s=head->next->next;
+    while(s) {
+        if(s->coef!=1 && s->coef!=-1) {
+            if(s->expn==0) printf("%d",s->coef);
+            else if(s->expn==1) printf("%dX",s->coef);
+            else printf("%dX^%d",s->coef,s->expn);
         }
-        if(q->coef != 1 && q->coef != -1) {
-            printf("%d", q->coef);
-            if(q->expn == 1) {
-                putchar('X');
-            } else if (q->expn) {
-                printf("X^%d", q->expn);
-            }
-        } else {
-            if(q->coef == 1) {
-                if(!q->expn) {
-                    putchar('1');
-                } else if (q->expn == 1) {
-                    putchar('X');
-                } else {
-                    printf("X^%d", q->expn);
-                }
-            }
-            if(q->coef == -1) {
-                if(!q->expn) {
-                    printf("-1");
-                } else if(q->expn == 1) {
-                    printf("-X");
-                } else {
-                    printf("-X^%d", q->expn);
-                }
+        else {
+            if(s->expn==0) printf("%d",s->coef);
+            else {
+                if(s->coef==-1) printf("-");
+                printf("X^%d",s->expn);
             }
         }
-        q = q->next;
-        flag++;
+        s=s->next;
+        if(s!=NULL && s->coef>0) printf("+");
     }
 }
-
+List CopyList (List head) {
+    List newHead = (List)malloc(sizeof(Node));
+    newHead->next = NULL;
+    List p = head->next;
+    List p2 = newHead;
+    while (p) {
+        List new = (List)malloc(sizeof(Node));
+        new->coef = p->coef;
+        new->expn = p->expn;
+        p2->next = new;
+        p2 = new;
+        p = p->next;
+    }
+    p2->next = NULL;
+    return newHead;
+}
 List Plus(List head1, List head2) {
     List pre = NULL, p = NULL, qre = NULL, q = NULL;
     pre = head1->next;
@@ -74,12 +81,13 @@ List Plus(List head1, List head2) {
     qre = head2->next;
     q = head2->next->next;
     int index = 1;
+    
     while (p != NULL) {
         if (q == NULL) {
             return head1;
         }
         if (p->expn == q->expn) {
-            p->coef = p->coef - q->coef;
+            p->coef = p->coef + q->coef;
             if (p->coef == 0) {
                 pre->next = p->next;
                 free(p);
@@ -106,18 +114,13 @@ List Plus(List head1, List head2) {
                     q = NULL;
                 }
             }
-//            printf("4\n");
-        } else if (p->expn > q->expn) {//多项式1的项的指数大于多项式2的项时
-            q->coef = -q->coef;
+        } else if (p->expn > q->expn) {
             qre->next = q->next;
             q->next = p;
             pre->next = q;
             pre = q;
             q = qre->next;
-//            printf("3\n");
-        } else if (q->expn > p->expn && p->next &&q->expn < (p->next)->expn) {//多项式2的项指数大小在多项式1的项与下一项中间时
-            q->coef = -q->coef;
-//            printf("1\n");
+        } else if (q->expn > p->expn && p->next &&q->expn < (p->next)->expn) {
             qre->next = q->next;
             pre = p;
             p = p->next;
@@ -128,31 +131,41 @@ List Plus(List head1, List head2) {
         } else if (q->expn > p->expn && p->next && q->expn >= (p->next)->expn) {
             pre = p;
             p = p->next;
- 
         } else {
             pre = p;
             break;
         }
         index ++;
     }
-    if (q) { 
-        pre->next = q;
-        List qq = q;
-        while(qq) {
-            qq->coef = - qq->coef;
-            qq = qq->next;
-        }
-    }
+    if (q) pre->next = q;
     return head1;
 }
-
-int main() {
-    int n1, n2;
-    List head1, head2;
-    scanf(" %d", &n1);
-    head1 = Creat(n1);
-    scanf("%d", &n2);
-    head2 = Creat(n2);
-    List head = Plus(head1, head2);
-    Print(head);
+List Multiply (List head1, List head2) {
+    
+    List p2 = head2->next->next;
+    
+    List newHead = (List)malloc(sizeof(Node));
+    List pp = (List)malloc(sizeof(Node));
+    newHead->next = pp;
+    
+    pp->next = NULL;
+    List p1;
+    List head1Pre = CopyList(head1);
+    List res = NULL;
+    while (p2) {
+        p1 = head1->next->next;
+        while (p1) {
+            
+            p1->coef *= p2->coef;
+            p1->expn += p2->expn;
+            p1 = p1->next;
+        }
+        
+        p2 = p2->next;
+        res = Plus(head1, newHead);
+        newHead = CopyList(head1);
+        head1 = CopyList(head1Pre);
+    }
+    return res;
 }
+    
